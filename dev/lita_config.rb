c0a71e127071937c7e0ee0073f3e9bdf6516e9ae
Tom Duffield <tom@chef.io>
2017-03-13 15:51:22 -0500

    Remove old development environment
    Signed-off-by: Tom Duffield <tom@chef.io>

--- dev/lita_config.rb
+++
@@ -1,7 +0,0 @@
-Lita.configure do |config|
-  config.robot.log_level = :debug
-  config.robot.adapter = :slack
-  config.robot.admins = ["1"]
-
-  config.adapters.slack.token = ENV["SLACK_TOKEN"]
-end
